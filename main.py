10222fc857b42d0be7c6511573cc7ab422b49075
diixo <anatolii.podoba@gmail.com>
2026-01-02 13:22:40 +0200

    main.py: removed items duplicates

--- main.py
+++ main.py
@@ -20,13 +20,33 @@ def get_token_encoder(tokenizer: str | None = None):
     return lambda s: len(enc.encode(s))
 
 
+def txt_to_jsonl(in_path: str, out_path: str, encoding: str = "utf-8") -> None:
+    with open(in_path, "r", encoding=encoding) as fin, open(out_path, "w", encoding=encoding) as fout:
+        for line in fin:
+            txt = line.strip()
+
+            if not txt:
+                continue
+
+            pos = txt.find(" is ")
+            subject = None
+            if pos > 0:
+                subject = txt[0:pos]
+
+            if subject is not None:
+                rec = {"verb": "", "meaning": f'definition of "{subject}"', "example": txt}
+            fout.write(json.dumps(rec, ensure_ascii=False) + "\n")
+
+
 def main():
 
+    #txt_to_jsonl("input.txt", "output.jsonl")
+    #exit(0)
+
     jsonl_path = "wordacy.jsonl"
-    field ="example"
 
     skip_nonstring = True
-
+    def_dict = dict()
 
     count_encoder = get_token_encoder()
 
@@ -38,7 +58,16 @@ def main():
             if not line:
                 continue
             obj = json.loads(line)
-            val = obj.get(field)
+
+            val = obj.get("example")
+            definition = obj.get("meaning")
+
+            if (definition is not None) and (definition != "") and (definition.find("definition") == 0):
+                if definition not in def_dict:
+                    def_dict[definition] = "meaning"
+                else:
+                    print(definition)
+
             if val is None or val == "":
                 continue
             if not isinstance(val, str):
